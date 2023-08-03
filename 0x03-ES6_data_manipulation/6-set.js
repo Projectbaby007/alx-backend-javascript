975f2e4febdb068acc9c5e870bd10ff4c043fe52
Projectbaby007 <orucaleb@gmail.com>
2023-08-03 20:47:15 +0000

    update and create all files properly

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Oru Caleb <https://github.com/Projectbaby007>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
